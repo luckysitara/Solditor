18a5a2125cf040d696fa5dd082048c61253d6440
luckysitara <udoessienprecious@gmail.com>
2025-06-06 08:12:17 -0700

    fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,9 @@
-import blitzPlugin from '@blitz/eslint-plugin';
-import { jsFileExtensions } from '@blitz/eslint-plugin/dist/configs/javascript.js';
-import { getNamingConventionRule, tsFileExtensions } from '@blitz/eslint-plugin/dist/configs/typescript.js';
-
 export default [
   {
     ignores: ['**/dist', '**/node_modules', '**/.wrangler', '**/bolt/build'],
   },
-  ...blitzPlugin.configs.recommended(),
   {
-    rules: {
-      '@blitz/catch-error-name': 'off',
-      '@typescript-eslint/no-this-alias': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-    },
-  },
-  {
-    files: ['**/*.tsx'],
-    rules: {
-      ...getNamingConventionRule({}, true),
-    },
-  },
-  {
-    files: ['**/*.d.ts'],
-    rules: {
-      '@typescript-eslint/no-empty-object-type': 'off',
-    },
-  },
-  {
-    files: [...tsFileExtensions, ...jsFileExtensions, '**/*.tsx'],
-    ignores: ['functions/*'],
+    files: ['**/*.tsx', '**/*.ts', '**/*.jsx', '**/*.js'],
     rules: {
       'no-restricted-imports': [
         'error',
